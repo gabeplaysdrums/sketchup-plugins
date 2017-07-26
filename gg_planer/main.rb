ca70c7e17ea3cf51aedd896b640f880a9db8fc47
Gabriel Young <gabeplaysdrums@live.com>
2017-07-25 21:49:55 -0700

    paint points with increased speed

--- gg_planer/main.rb
+++ gg_planer/main.rb
@@ -7,19 +7,19 @@ menu.add_item("Planer") { Sketchup.active_model.select_tool(PlanerTool.new) }
 menu.add_item("Planer Settings") { PlanerTool.show_settings }
 
 class PlanerTool
-    @@max_distance = '5'.to_l
     @@normal_length = '10'.to_l
-    @@max_vertex_hops = 5
+    @@brush_radius = '0'.to_l
+    @@brush_hops = 5
 
     def self.show_settings
-        prompts = ['Max distance', 'Normal length', 'Max hops']
-        defaults = [@@max_distance, @@normal_length, @@max_vertex_hops]
+        prompts = ['Normal length', 'Brush radius', 'Brush hops']
+        defaults = [@@normal_length, @@brush_radius, @@brush_hops]
         input = UI.inputbox(prompts, defaults, 'Planer Options')
 
         if input
-            @@max_distance = input[0].to_l
-            @@normal_length = input[1].to_l
-            @@max_vertex_hops = input[2].to_i
+            @@normal_length = input[0].to_l
+            @@brush_radius = input[1].to_l
+            @@brush_hops = input[2].to_i
         end
     end
 
@@ -28,14 +28,10 @@ class PlanerTool
 
         @ph = Sketchup.active_model.active_view.pick_helper
 
-        Sketchup::set_status_text('Select a point to generate plane', SB_PROMPT)
+        Sketchup::set_status_text('Click a vertex to start painting vertices', SB_PROMPT)
         @originInput = Sketchup::InputPoint.new
 
-        @points = []
-        @vertices = Set.new
-        @centroid = nil
-        @projected_centroid = nil
-        @normal = nil
+        self.reset_plane
     end
 
     def deactivate(view)
@@ -44,77 +40,91 @@ class PlanerTool
         view.invalidate if view
     end
 
+    def reset_plane
+        @points = []
+        @vertices = Set.new
+        @centroid = nil
+        @projected_centroid = nil
+        @normal = nil
+
+        Sketchup::set_status_text('Click a vertex to start painting vertices', SB_PROMPT)
+    end
+
     def onLButtonDown(flags, x, y, view)
-        puts "onLButtonDown: flags = #{flags}"
-        puts "                   x = #{x}"
-        puts "                   y = #{y}"
-        puts "                view = #{view}"
-
-        if (Sketchup.active_model.selection.length == 0)
-            Sketchup.active_model.selection.clear
-            @ph.do_pick(x, y)
-            picked = @ph.best_picked
-            Sketchup.active_model.selection.add(picked) if picked
-        end
-
-        #Do nothing with this click if nothing is selected
-        return nil if Sketchup.active_model.selection.length == 0
-
+        # Commit action by clicking on a vertex
         @originInput.pick view, x, y
+        return unless (@originInput.valid? and @originInput.degrees_of_freedom == 0 and @originInput.vertex)
+        view.invalidate
 
-        if @originInput.valid?
-            origin = @originInput.position
+        first_point = @points.empty?
+        self.add_to_plane(@originInput.vertex, @originInput.transformation, view)
 
-            return if not @originInput.vertex
-            points = find_vertex_points_max_hops_from_vertex(@originInput.vertex, @originInput.transformation, @@max_vertex_hops)
-
-            #points = find_vertex_points_max_distance_from_point(Sketchup.active_model.selection, origin, @@max_distance)
-            puts 'found %d points nearby' % [points.length]
-            return if points.length == 0
-
-            plane = Geom.fit_plane_to_points(points.entries)
-            projected_origin = origin.project_to_plane plane
-            p, v = normalize_plane plane
-            puts 'plane normal: %s' % [v]
-            v.length = @@normal_length
-
-            if v.z < 0
-                v = v.reverse
-            end
-
-            Sketchup.active_model.active_entities.add_line(projected_origin, projected_origin.offset(v))
+        if first_point
+            # Begin painting vertices
+            Sketchup::set_status_text('Move mouse to paint vertices to plane.  Click a vertex to commit the plane', SB_PROMPT)
+        elsif @points.length > 3
+            # Commit plane
+            Sketchup.active_model.active_entities.add_line(@projected_centroid, @projected_centroid.offset(@normal))
+            self.reset_plane
         end
     end
 
     def onMouseMove(flags, x, y, view)
         @originInput.pick view, x, y
-        if @originInput.degrees_of_freedom == 0 and @originInput.vertex
-            view.invalidate
+        return unless (@originInput.valid? and @originInput.degrees_of_freedom == 0 and @originInput.vertex)
+        view.invalidate
 
-            if not @vertices.include?(@originInput.vertex)
-                @vertices.add(@originInput.vertex)
-                @points.push(@originInput.position)
+        return if @points.empty?
+        self.add_to_plane(@originInput.vertex, @originInput.transformation, view)
+    end
 
-                # Compute the new centroid
-                @centroid = Geom::Point3d.new 0, 0, 0
-                @points.each { |p| @centroid = @centroid + [p.x, p.y, p.z] }
-                @centroid = Geom::Point3d.new(@centroid.x / @points.length, @centroid.y / @points.length, @centroid.z / @points.length)
+    def add_to_plane(vertex, transformation, view)
+        found_vertices = self.find_vertices_near(vertex, @@brush_hops)
+        puts 'found %d vertices nearby' % [found_vertices.length]
 
-                # Compute new plane
-                plane = Geom.fit_plane_to_points(@points)
-                @projected_centroid = @centroid.project_to_plane plane
-                p, @normal = normalize_plane plane
-                @normal.length = @@normal_length
-
-                if @normal.z < 0
-                    @normal = @normal.reverse
-                end
+        points_added = false
+        found_vertices.entries.each { |v|
+            if not @vertices.include?(v)
+                @vertices.add(v)
+                @points.push(transformation * v.position)
+                points_added = true
             end
+        }
+
+        return unless points_added
+
+        view.invalidate
+
+        return if @points.length < 3
+
+        # Compute the new centroid
+        @centroid = Geom::Point3d.new 0, 0, 0
+        @points.each { |p| @centroid = @centroid + [p.x, p.y, p.z] }
+        @centroid = Geom::Point3d.new(@centroid.x / @points.length, @centroid.y / @points.length, @centroid.z / @points.length)
+
+        # Compute new plane
+        plane = Geom.fit_plane_to_points(@points)
+        @projected_centroid = @centroid.project_to_plane plane
+        p, @normal = normalize_plane plane
+        @normal.length = @@normal_length
+
+        if @normal.z < 0
+            @normal = @normal.reverse
         end
+    end
+
+    def pick_and_add_vertex_point(view, x, y)
+        @originInput.pick view, x, y
+        return unless (@originInput.valid? and @originInput.degrees_of_freedom == 0 and @originInput.vertex)
+        view.invalidate
+
+        self.add_to_plane(@originInput.vertex, @originInput.transformation, view)
     end
 
     def draw(view)
         @originInput.draw view
+
+        return if @points.empty?
         view.draw_points(@points, 10, 5, 'green')
 
         if @projected_centroid and @normal
@@ -125,40 +135,26 @@ class PlanerTool
         end
     end
 
-    def find_vertex_points_max_distance_from_point(entities, from_point, max_distance, transformation = Geom::Transformation.new)
-        points = Set.new
-        known_vertices = Set.new
-        entities.each { |e|
-            if e.public_methods.include?(:entities)
-                points += self.find_vertex_points_max_distance_from_point(e.entities, from_point, max_distance, transformation * e.transformation)
-            elsif e.is_a?(Sketchup::ComponentInstance)
-                points += self.find_vertex_points_max_distance_from_point(e.definition.entities, from_point, max_distance, transformation * e.transformation)
-            elsif e.is_a?(Sketchup::Edge)
-                e.vertices.each { |v|
-                    if not known_vertices.include?(v)
-                        known_vertices.add(v)
-                        point = transformation * v.position
-                        points.add(point) if from_point.distance(point) < max_distance
-                    end
-                }
-            end
-        }
-        return points
-    end
-
-    def find_vertex_points_max_hops_from_vertex(from_vertex, transformation, max_hops, known_vertices = Set.new)
+    def find_vertices_near(origin_vertex, max_hops, from_vertex = nil, known_vertices = Set.new)
+        from_vertex = origin_vertex if not from_vertex
         return Set.new if (max_hops == 0 or known_vertices.include?(from_vertex))
 
         known_vertices.add(from_vertex)
-        points = Set.new([ transformation * from_vertex.position ])
+        found_vertices = Set.new
 
-        from_vertex.edges.each { |e|
-            e.vertices.each { |v|
-                points += self.find_vertex_points_max_hops_from_vertex(v, transformation, max_hops - 1, known_vertices)
+        if from_vertex.position.distance(origin_vertex.position) <= @@brush_radius
+            found_vertices.add(from_vertex)
+        end
+
+        if @@brush_radius > 0
+            from_vertex.edges.each { |e|
+                e.vertices.each { |v|
+                    found_vertices += self.find_vertices_near(origin_vertex, max_hops - 1, v, known_vertices)
+                }
             }
-        }
+        end
 
-        return points
+        return found_vertices
     end
 
     def normalize_plane(plane)
