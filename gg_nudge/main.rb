aaee77521dcbe48bd6b778289dec6ec517ae7190
Gabriel Young <gabeplaysdrums@live.com>
2017-07-30 13:07:48 -0700

    initial Nudge prototype

---
+++ gg_nudge/main.rb
@@ -0,0 +1,138 @@
+module GG_Nudge
+
+menu = UI.menu("Tools")
+menu.add_item("Nudge") { Sketchup.active_model.select_tool(NudgeTool.new) }
+
+KC_UP = 63232
+KC_DOWN = 63233
+KC_LEFT = 63234
+KC_RIGHT = 63235
+KC_HOME = 63273
+KC_END = 63275
+TRANSLATE_STEP = '0.25'.to_l
+ROTATE_STEP = 0.25.degrees
+
+class NudgeTool
+
+    def activate
+        @ph = Sketchup.active_model.active_view.pick_helper
+        puts 'Nudge activated'
+
+        Sketchup::set_status_text('Choose an origin point', SB_PROMPT)
+        @originInput = Sketchup::InputPoint.new
+        @origin = nil
+
+    end
+
+    def start_move
+        Sketchup::set_status_text('Press arrow keys to nudge', SB_PROMPT)
+    end
+
+    def deactivate(view)
+        puts 'Nudge deactivated'
+
+        view.invalidate if view
+    end
+
+    def onKeyDown(key, repeat, flags, view)
+        puts "onKeyDown: key = #{key}"
+        puts "        repeat = #{repeat}"
+        puts "         flags = #{flags}"
+        puts "          view = #{view}"
+
+        translate = (flags & ALT_MODIFIER_MASK == 0)
+        y_axis = (flags & COPY_MODIFIER_MASK == 0)
+
+        if translate
+            step = (flags & CONSTRAIN_MODIFIER_MASK == 0) ? TRANSLATE_STEP : 10*TRANSLATE_STEP
+
+            if false
+            elsif key == KC_RIGHT
+                puts '+x'
+                self.translate(step, 0, 0)
+            elsif key == KC_LEFT
+                puts '-x'
+                self.translate(-step, 0, 0)
+            elsif key == KC_UP and y_axis
+                puts '+y'
+                self.translate(0, step, 0)
+            elsif key == KC_DOWN and y_axis
+                puts '-y'
+                self.translate(0, -step, 0)
+            elsif ((key == KC_UP and not y_axis) or key == KC_HOME)
+                puts '+z'
+                self.translate(0, 0, step)
+            elsif ((key == KC_DOWN and not y_axis) or key == KC_END)
+                puts '-z'
+                self.translate(0, 0, -step)
+            end
+        else
+            step = (flags & CONSTRAIN_MODIFIER_MASK == 0) ? ROTATE_STEP : 10*ROTATE_STEP
+
+            if false
+            elsif key == KC_RIGHT
+                puts '+x'
+                self.rotate(step, 1, 0, 0)
+            elsif key == KC_LEFT
+                puts '-x'
+                self.rotate(step, -1, 0, 0)
+            elsif key == KC_UP and y_axis
+                puts '+y'
+                self.rotate(step, 0, 1, 0)
+            elsif key == KC_DOWN and y_axis
+                puts '-y'
+                self.rotate(step, 0, -1, 0)
+            elsif ((key == KC_UP and not y_axis) or key == KC_HOME)
+                puts '+z'
+                self.rotate(step, 0, 0, 1)
+            elsif ((key == KC_DOWN and not y_axis) or key == KC_END)
+                puts '-z'
+                self.rotate(step, 0, 0, -1)
+            end
+        end
+    end
+
+    def translate(x, y, z)
+        return if not @origin
+
+        t = Geom::Vector3d.new(x, y, z)
+        @origin = @origin + t
+
+        Sketchup.active_model.selection.each { |entity|
+            entity.transform!(Geom::Transformation.translation(t))
+        }
+    end
+
+    def rotate(angle, x, y, z)
+        return if not @origin
+        Sketchup.active_model.selection.each { |entity|
+            entity.transform!(Geom::Transformation.rotation(@origin, Geom::Vector3d.new(x, y, z), angle))
+        }
+    end
+
+    def onLButtonDown(flags, x, y, view)
+        if (Sketchup.active_model.selection.length == 0)
+            #Clear selection
+            Sketchup.active_model.selection.clear
+
+            #Select what's hovered
+            @ph.do_pick(x, y)
+            picked = @ph.best_picked
+            if (picked)
+                Sketchup.active_model.selection.add picked
+            end
+        end
+
+        #Do nothing with this click if nothing is selected
+        return nil if Sketchup.active_model.selection.length == 0
+
+        @originInput.pick view, x, y
+        @origin = (@originInput.valid?) ? @originInput.position : nil
+
+        if @origin
+            Sketchup::set_status_text("Use keyboard to nudge selection", SB_PROMPT)
+        end
+    end
+
+end#class
+end#module
